37a9bdd694cddd5b0ecbdfd711507788b49aea79
TheSkittleschan <theskittleschan@gmail.com>
2022-03-07 16:25:28 -0600

    fix: Fixes TestRationalClass

--- lib/src/test/java/JavaGradle/TestRationalClass.java
+++ lib/src/test/java/JavaGradle/TestRationalClass.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 
 public class TestRationalClass {
     @Test
-    public void testRational {
+    public void testRational() {
         Rational r1 = new Rational(4,2);
         Rational r2 = new Rational(2,3);
 
-        System.out.println(r1 + " + " + r2 + " = " r1.add(r2));
+        System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
         System.out.println(r1 + " - " + r2 + " = " + r1.subtract(r2));
         System.out.println(r1 + " * " + r2 + " = " + r1.multiply(r2));
         System.out.println(r1 + " / " + r2 + " = " + r1.divide(r2));
